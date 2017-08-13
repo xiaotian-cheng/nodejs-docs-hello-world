cb3b7754552cb019efa7bea23ee67013acada11c
unknown <twinge.cheng@gmail.com>
2017-08-13 16:41:56 +0800

    commit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-
 });
 
 var port = process.env.PORT || 1337;
